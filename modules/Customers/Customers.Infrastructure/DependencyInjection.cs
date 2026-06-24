e4cfe03ae3819bc85e6bf51a92bcf2a5fa764fe8
Nurman Hadi <manzdeveloper@gmail.com>
2026-06-24 20:43:53 +0700

    update di

--- modules/Customers/Customers.Infrastructure/DependencyInjection.cs
+++ modules/Customers/Customers.Infrastructure/DependencyInjection.cs
@@ -1,24 +1,14 @@
-﻿using System.Reflection;
-using Customers.Core;
+﻿using Customers.Core;
 using Customers.Infrastructure.Data;
 using Customers.Infrastructure.Repositories;
-using FluentMigrator.Runner;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Customers.Infrastructure;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddCustomersInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    public static IServiceCollection AddCustomersInfrastructure(this IServiceCollection services)
     {
-        services.AddFluentMigratorCore()
-        .ConfigureRunner(rb => rb
-                .AddMySql()
-                .WithGlobalConnectionString(configuration.GetConnectionString("DefaultConnection"))
-                .ScanIn(Assembly.GetExecutingAssembly()).For.Migrations())
-            .AddLogging(lb => lb.AddFluentMigratorConsole());
-
         services.AddScoped<ICustomerConnectionFactory, CustomerConnection>();
         services.AddScoped<ICustomerRepository, CustomerRepository>();
         services.AddScoped<ICustomerService, CustomerService>();
