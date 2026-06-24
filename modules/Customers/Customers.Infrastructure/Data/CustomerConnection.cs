72b511ef841ecfc55a9d8060c7b2def80f5e945c
Nurman Hadi <manzdeveloper@gmail.com>
2026-06-24 11:59:04 +0700

    fix primary constructor

--- modules/Customers/Customers.Infrastructure/Data/CustomerConnection.cs
+++ modules/Customers/Customers.Infrastructure/Data/CustomerConnection.cs
@@ -1,12 +1,13 @@
 using System.Data;
+using Microsoft.Extensions.Configuration;
 using MySql.Data.MySqlClient;
 
 namespace Customers.Infrastructure.Data;
 
-public class CustomerConnection(string connectionString) : ICustomerConnectionFactory
+public class CustomerConnection(IConfiguration configuration) : ICustomerConnectionFactory
 {
     public IDbConnection CustomerCreateConnection()
     {
-        return new MySqlConnection(connectionString);
+        return new MySqlConnection(configuration.GetConnectionString("DefaultConnection"));
     }
 }
