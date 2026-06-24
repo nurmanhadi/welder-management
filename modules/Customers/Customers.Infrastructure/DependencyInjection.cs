ed0395f106a8af35ffae1b4702621c7ce944455c
Nurman Hadi <manzdeveloper@gmail.com>
2026-06-24 11:59:24 +0700

    add infra DI

---
+++ modules/Customers/Customers.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,27 @@
+﻿using System.Reflection;
+using Customers.Core;
+using Customers.Infrastructure.Data;
+using Customers.Infrastructure.Repositories;
+using FluentMigrator.Runner;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Customers.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddCustomersInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddFluentMigratorCore()
+        .ConfigureRunner(rb => rb
+                .AddMySql()
+                .WithGlobalConnectionString(configuration.GetConnectionString("DefaultConnection"))
+                .ScanIn(Assembly.GetExecutingAssembly()).For.Migrations())
+            .AddLogging(lb => lb.AddFluentMigratorConsole());
+
+        services.AddScoped<ICustomerConnectionFactory, CustomerConnection>();
+        services.AddScoped<ICustomerRepository, CustomerRepository>();
+        services.AddScoped<ICustomerService, CustomerService>();
+        return services;
+    }
+}
