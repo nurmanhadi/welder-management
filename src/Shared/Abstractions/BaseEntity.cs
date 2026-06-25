391c928667042d34984002b9592729d744c6663d
Nurman Hadi <manzdeveloper@gmail.com>
2026-06-25 17:37:15 +0700

    add property search index

--- src/Shared/Abstractions/BaseEntity.cs
+++ src/Shared/Abstractions/BaseEntity.cs
@@ -3,6 +3,7 @@ namespace Shared.Abstractions;
 public abstract class BaseEntity
 {
     public Guid Id { get; set; } = Guid.NewGuid();
+    public string? SearchIndex { get; set; }
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     public DateTime? DeletedAt { get; set; } = null;
 }
