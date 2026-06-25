19dd7715bf3c6f88fd83973d692ed8ebc1db9975
Nurman Hadi <manzdeveloper@gmail.com>
2026-06-25 21:05:47 +0700

    add repo implement

--- modules/Customers/Customers.Infrastructure/Repositories/CustomerRepository.cs
+++ modules/Customers/Customers.Infrastructure/Repositories/CustomerRepository.cs
@@ -1,6 +1,8 @@
 using Customers.Core;
 using Customers.Infrastructure.Data;
 using Dapper;
+using Shared.Responses;
+using Shared.Tools;
 
 namespace Customers.Infrastructure.Repositories;
 
@@ -85,6 +87,51 @@ public class CustomerRepository(ICustomerConnectionFactory connFactory) : ICusto
         using var conn = connFactory.CustomerCreateConnection();
         var result = await conn.QueryFirstOrDefaultAsync<Customer>(sql, parameters);
         return result;
+    }
+
+    public async Task<Pagination<Customer>> GetCustomersAsync(CustomerFilter filter)
+    {
+        var whereClause = "WHERE deleted_at IS NULL";
+        string? searchKeyword = null;
+        if (!string.IsNullOrEmpty(filter.Search))
+        {
+            searchKeyword = filter.Search.ToNgram();
+            whereClause += " AND MATCH(search_index) AGAINST(@Keyword IN BOOLEAN MODE)";
+        }
+        var direction = filter.Direction == Direction.Ascending ? "ASC" : "DESC";
+        var sortBy = filter.Sort switch
+        {
+            CustomerSort.Name => $"ORDER BY name {direction}",
+            CustomerSort.Phone => $"ORDER BY phone {direction}",
+            CustomerSort.CreatedAt => $"ORDER BY created_at {direction}",
+            _ => $"ORDER BY created_at {direction}"
+        };
+
+        List<string> queries = [];
+        queries.Add($"""
+            SELECT id, name, phone, address
+            FROM customers
+            {whereClause}
+            {sortBy}
+            LIMIT @Limit OFFSET @Offset;
+            """);
+        queries.Add($"SELECT COUNT(id) FROM customers {whereClause};");
+
+        var sql = string.Join(' ', queries);
+        var parameters = new DynamicParameters();
+        parameters.Add("Limit", filter.PageSize);
+        parameters.Add("Offset", (filter.Page - 1) * filter.PageSize);
+        if (searchKeyword != null)
+        {
+            parameters.Add("Keyword", searchKeyword);
+        }
+
+        using var conn = connFactory.CustomerCreateConnection();
+        var multi = await conn.QueryMultipleAsync(sql, parameters);
+        var customers = await multi.ReadAsync<Customer>();
+        var totalItems = await multi.ReadSingleAsync<int>();
+        var contents = customers.ToList().AsReadOnly();
+        return new Pagination<Customer>(contents, filter.Page, filter.PageSize, totalItems);
     }
 
     public async Task UpdateAsync(Customer entity)
