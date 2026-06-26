2a33f45bfd9ae05e129cde5c4dce921dc8ceb6d9
Nurman Hadi <manzdeveloper@gmail.com>
2026-06-26 18:41:07 +0700

    update base entity

--- src/Shared/Abstractions/BaseEntity.cs
+++ src/Shared/Abstractions/BaseEntity.cs
@@ -3,7 +3,7 @@ namespace Shared.Abstractions;
 public abstract class BaseEntity
 {
     public Guid Id { get; set; } = Guid.NewGuid();
-    public string? SearchIndex { get; set; }
+    public string SearchIndex { get; set; } = string.Empty;
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     public DateTime? DeletedAt { get; set; } = null;
 }
