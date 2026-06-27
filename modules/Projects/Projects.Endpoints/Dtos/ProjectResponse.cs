fdc0ba9fce1f8b36a8b0f5f940499631af125758
Nurman Hadi <manzdeveloper@gmail.com>
2026-06-28 00:14:28 +0700

    update dto

--- modules/Projects/Projects.Endpoints/Dtos/ProjectResponse.cs
+++ modules/Projects/Projects.Endpoints/Dtos/ProjectResponse.cs
@@ -6,12 +6,11 @@ namespace Projects.Endpoints.Dtos;
 public record ProjectResponse(
     [property: JsonPropertyName("id")] Guid Id,
     [property: JsonPropertyName("pid")] Guid PId,
-    [property: JsonPropertyName("customer_id")] Guid CustomerId,
     [property: JsonPropertyName("title")] string Title,
     [property: JsonPropertyName("description")] string Description,
     [property: JsonPropertyName("cost")] decimal Cost,
     [property: JsonPropertyName("status")] Status Status,
     [property: JsonPropertyName("start_date")] DateTime? StartDate,
     [property: JsonPropertyName("end_date")] DateTime? EndDate,
-    [property: JsonPropertyName("end_date")] CustomerResponse Customer
+    [property: JsonPropertyName("customer")] CustomerResponse Customer
 );
