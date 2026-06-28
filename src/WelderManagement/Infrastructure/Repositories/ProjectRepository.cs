1cd5afaa03ac3e3da0d6f590b249b396749d2ccc
Nurman Hadi <manzdeveloper@gmail.com>
2026-06-28 20:46:45 +0700

    add more implement repo

--- src/WelderManagement/Infrastructure/Repositories/ProjectRepository.cs
+++ src/WelderManagement/Infrastructure/Repositories/ProjectRepository.cs
@@ -8,19 +8,48 @@ namespace WelderManagement.Infrastructure.Repositories;
 
 public class ProjectRepository(IConnectionFactory _db) : IProjectRepository
 {
-    public Task<int> CountByIdAsync(Guid id)
+    public async Task<int> CountByIdAsync(Guid id)
     {
-        throw new NotImplementedException();
+        var sql = """
+        SELECT COUNT(id)
+        FROM projects
+        WHERE deleted_at IS NULL AND id = $Id
+        """;
+        var parameters = new DynamicParameters();
+        parameters.Add("Id", id);
+
+        using var conn = _db.ProjectCreateConnection();
+        int result = await conn.ExecuteScalarAsync<int>(sql, parameters);
+        return result;
     }
 
-    public Task DeleteAsync(Guid id)
+    public async Task DeleteAsync(Guid id)
     {
-        throw new NotImplementedException();
+        var sql = """
+        UPDATE projects SET deleted_at = @DeletedAt
+        WHERE id = $Id
+        """;
+        var parameters = new DynamicParameters();
+        parameters.Add("DeletedAt", DateTime.UtcNow);
+        parameters.Add("Id", id);
+
+        using var conn = _db.ProjectCreateConnection();
+        await conn.ExecuteAsync(sql, parameters);
     }
 
-    public Task<Project?> GetByIdAsync(Guid id)
+    public async Task<Project?> GetByIdAsync(Guid id)
     {
-        throw new NotImplementedException();
+        var sql = """
+        SELECT id, pid, customer_id, title, description, cost, status, start_date, end_date
+        FROM projects
+        WHERE deleted_at IS NULL AND id = $Id
+        """;
+        var parameters = new DynamicParameters();
+        parameters.Add("Id", id);
+
+        using var conn = _db.ProjectCreateConnection();
+        var result = await conn.QueryFirstOrDefaultAsync<Project>(sql, parameters);
+        return result;
     }
 
     public async Task InsertAsync(Project entity)
@@ -45,8 +74,23 @@ public class ProjectRepository(IConnectionFactory _db) : IProjectRepository
         await conn.ExecuteAsync(sql, parameters);
     }
 
-    public Task UpdateAsync(Project entity)
+    public async Task UpdateAsync(Project entity)
     {
-        throw new NotImplementedException();
+        var keyword = $"{entity.PID} {entity.Title}";
+        var searchIndex = keyword.ToNgram();
+        var sql = """
+        UPDATE projects SET title = @Title, description = @Description, cost = @Cost, status = @Status, search_index = @SearchIndex
+        WHERE id = @Id
+        """;
+        var parameters = new DynamicParameters();
+        parameters.Add("Title", entity.Title);
+        parameters.Add("Description", entity.Description);
+        parameters.Add("Cost", entity.Cost);
+        parameters.Add("Status", entity.Status);
+        parameters.Add("SearchIndex", searchIndex);
+        parameters.Add("Id", entity.Id);
+
+        using var conn = _db.ProjectCreateConnection();
+        await conn.ExecuteAsync(sql, parameters);
     }
 }
